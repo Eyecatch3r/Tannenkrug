fcb791a24374a102bda4f5ef1cabe07c927e021a
Aldo Costa <aldo.costa@web.de>
2023-09-15 18:54:50 +0200

    -first stable release of home page with Menu button

---
+++ app/components/Logo.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from "react";
+import styled from 'styled-components';
+import { useTheme } from 'next-themes'; // Import the useTheme hook from next-themes
+
+export function Logo() {
+    const Header = styled.header`
+    font-family: 'Josefin Sans', sans-serif;
+    background-color: rgba(51, 51, 51, 0);
+    padding: 20px;
+    text-align: center;
+    // Add the text-white class for white text in dark mode 
+    ${props => props.theme === 'dark' ? 'text-white' : 'text-black'}
+  `;
+
+    const Title = styled.h1`
+    font-size: 2rem;
+  `;
+
+    const { resolvedTheme } = useTheme(); // Use the useTheme hook to get the current theme
+
+    return (
+        <Header theme={resolvedTheme}><Title>TANNENKRUG</Title></Header>
+    );
+}
