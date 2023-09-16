04c849a773a7cc7389e0729d084723beb3034d1f
Aldo Costa <aldo.costa@web.de>
2023-09-16 13:55:29 +0200

    -fixed page.tsx import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import React from 'react';
 import Link from "next/link";
-import styles from "./page.css"
 
 export default function Home() {
   let pdfUrl = 'Speisekarte.pdf';
